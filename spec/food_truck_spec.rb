f7fb7298069980db6ecbb4d8ca660ebc75ec5798
tjhaines-cap <tjhaines@eckerd.edu>
2022-04-26 09:13:01 -0600

    Feat: test and create food truck

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -1,1 +1,13 @@
 require './lib/item'
+require './lib/food_truck'
+
+describe FoodTruck do
+
+  it 'exists and has readable attributes' do
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    expect(food_truck).to be_a(FoodTruck)
+    expect(food_truck.name).to eq("Rocky Mountain Pies")
+    expect(food_truck.inventory).to eq({})
+  end
+
+end
