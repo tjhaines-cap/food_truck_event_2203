3f3ec9d7b761b68614fda9b21feae31c6f084f73
tjhaines-cap <tjhaines@eckerd.edu>
2022-04-26 09:27:17 -0600

    Feat: test and implement stock and check stock

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -1,12 +1,28 @@
-
+require 'pry'
 
 class FoodTruck
 
   attr_reader :name, :inventory
-  
+
   def initialize(name)
     @name = name
     @inventory = {}
+  end
+
+  def check_stock(item)
+    item_stock = 0
+    if @inventory[item]
+      item_stock = @inventory[item]
+    end
+    return item_stock
+  end
+
+  def stock(item, amount)
+    if @inventory[item]
+      @inventory[item] += amount
+    else
+      @inventory[item] = amount
+    end
   end
 
 end
